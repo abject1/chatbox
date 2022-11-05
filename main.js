39650f6ebe214671bbb65934ad41fea36b62366e
lucas wurtz <lucaswurtz0@gmail.com>
2022-11-05 16:58:37 +0100

    fin du projet

--- main.js
+++ main.js
@@ -3,20 +3,10 @@ const textArea = document.getElementById('textArea');
 const fenetre = document.getElementById('window');
 let a;
 let b = 0;
-let c = 0;
-let d = 0;
 
 const message = [
-    "Très bien j'aurais bessoin de plus d'information dans ce cas."
+    "Très bien j'aurais bessoin de plus d'information dans ce cas.","Pour cette utilisation je vous conseil un I7-9700k ainsi que 16go de ram en 3000Mhz.","Aucun soucit je reste a votre disposition pour toute autres question, bonne journée à vous."
     
-]
-
-const message2 = [
-    "Pour cette utilisation je vous conseil un I7-9700k ainsi que 16go de ram en 3000Mhz."
-]
-
-const message3 = [
-    "Aucun soucit je reste a votre disposition pour toute autres question, bonne journée à vous."
 ]
 
 const getCharacter = async () => {
@@ -34,15 +24,12 @@ const getCharacter = async () => {
 
 const sendMessage = async () => {
     try {
-        if (b == 1) {
         fenetre.innerHTML += `
         <div class="message">
             <img src="./avatar_1.jpg" alt="avatar_1">
-            <p>${message}</p>
+            <p>${message[0]}</p>
         </div>
         `
-        }
-
     } catch (error) {
         throw new Error(error.message || error);
     }
@@ -50,31 +37,24 @@ const sendMessage = async () => {
 
 const sendMessage2 = async () => {
     try {
-        if (c == 2) {
         fenetre.innerHTML += `
         <div class="message">
             <img src="./avatar_1.jpg" alt="avatar_1">
-            <p>${message2}</p>
+            <p>${message[1]}</p>
         </div>
         `
-        }
-
     } catch (error) {
         throw new Error(error.message || error);
     }
 }
-
 const sendMessage3 = async () => {
     try {
-        if (d == 3) {
         fenetre.innerHTML += `
         <div class="message">
             <img src="./avatar_1.jpg" alt="avatar_1">
-            <p>${message3}</p>
+            <p>${message[2]}</p>
         </div>
         `
-        }
-
     } catch (error) {
         throw new Error(error.message || error);
     }
@@ -84,17 +64,13 @@ btnSend.addEventListener('click', () => {
     a = textArea.value
     getCharacter();
     textArea.value = "";
-    for (b = 0; b < 1; b++) {
+    b++;
+    if (b === 1) {
         sendMessage();
-        c++;
-        d++;
-    };
-    for (c = 0; c < 2; c++) {
-        sendMessage2()
-        d++;
-    }
-    for (d = 3; d < 3; c++) {
-        sendMessage3()
+    } else if (b === 2) {
+        sendMessage2();
+    } else if (b === 3) {
+        sendMessage3();
     }
 })
 
